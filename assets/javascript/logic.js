5d11b35706f51244ebeed7d8b761d20f05c1a6c8
orpillasam <31043722+orpillasam@users.noreply.github.com>
2017-11-07 16:44:33 -0800

    Add files via upload

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDEuPV_lRVJwT7Revku-rRBuRupZ1dB_tM",
+    authDomain: "train-scheduler-6329e.firebaseapp.com",
+    databaseURL: "https://train-scheduler-6329e.firebaseio.com",
+    projectId: "train-scheduler-6329e",
+    storageBucket: "train-scheduler-6329e.appspot.com",
+    messagingSenderId: "904116934815"
+  };
+  firebase.initializeApp(config);
